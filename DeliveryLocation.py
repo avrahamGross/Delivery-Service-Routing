ce7dcbd1317f9d92a04532555ed3a7ae6e23736d
Avraham Gross <avrahamgross5@gmail.com>
2022-09-06 12:19:46 -0400

    added comments for class

--- DeliveryLocation.py
+++ DeliveryLocation.py
@@ -1,3 +1,5 @@
+# This class is used in two distinct ways. A Delivery Location can be a location with a certain distance to get there,
+# or a location with a list of all available locations to visit next
 class DeliveryLocation:
 
     def __init__(self, name, distance = None, location_list = None):
